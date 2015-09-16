12031b31577b32658d510978db40cfde8778783b
Andrey Zinenko <azinenko@spbtv.com>
2015-09-16 16:46:00 +0300

    Add AWS region to carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,9 +11,11 @@ CarrierWave.configure do |config|
       provider: "AWS",
       aws_access_key_id: ENV["S3_KEY"],
       aws_secret_access_key: ENV["S3_SECRET"],
+      region: ENV["S3_REGION"]
     }
 
-    config.fog_directory = "piter-united-#{Rails.env}"
+    config.fog_directory = ENV["S3_BUCKET"]
+    config.fog_public = true
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
   end
 
