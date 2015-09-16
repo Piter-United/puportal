418b8c4afe5cbbb806afc6c1d11738227c198761
Andrey Zinenko <azinenko@spbtv.com>
2015-09-16 16:48:25 +0300

    Tmp comment community covers

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Community.create!(
   hashtag: "spm",
   description: "Петербургский клуб менеджеров проектов в IT.",
   links: "https://plus.google.com/u/0/communities/108809460896106613736",
-  remote_cover_url: "https://lh6.googleusercontent.com/-BOQdjEhr_1w/VJFAp9q13LI/AAAAAAAATA8/4lt7iUbR2WA/w1906-h1276-no/2014-12-16%2B02.54.19.jpg",
+  # remote_cover_url: "https://lh6.googleusercontent.com/-BOQdjEhr_1w/VJFAp9q13LI/AAAAAAAATA8/4lt7iUbR2WA/w1906-h1276-no/2014-12-16%2B02.54.19.jpg",
   remote_logo_url: "http://piter-united.ru/img/spmclub.png",
 )
 
@@ -14,7 +14,7 @@ Community.create!(
   hashtag: "agile-piter",
   description: "Сообщество интересующихся гибкими подходами к разработке.",
   links: "https://plus.google.com/u/0/communities/112264824472767454602",
-  remote_cover_url: "https://lh4.googleusercontent.com/-7yb3l4YAB6c/U7xB7OlDgzI/AAAAAAAABCs/mgDdUs8EC0o/w1702-h1276-no/IMG_0661.jpg",
+  # remote_cover_url: "https://lh4.googleusercontent.com/-7yb3l4YAB6c/U7xB7OlDgzI/AAAAAAAABCs/mgDdUs8EC0o/w1702-h1276-no/IMG_0661.jpg",
   remote_logo_url: "http://piter-united.ru/img/agilepiter.png",
 )
 
@@ -24,7 +24,7 @@ Community.create!(
   hashtag: "spb-coa",
   description: "Сообщество аналитиков Санкт-Петербурга.",
   links: "https://vk.com/spb_analytics",
-  remote_cover_url: "https://pp.vk.me/c312920/v312920184/831f/7n18iXdFMa4.jpg",
+  # remote_cover_url: "https://pp.vk.me/c312920/v312920184/831f/7n18iXdFMa4.jpg",
   remote_logo_url: "http://piter-united.ru/img/spbcoa.png"
 )
 
@@ -34,7 +34,7 @@ Community.create!(
   hashtag: "spb-sqa",
   description: "Сообщество тестировщиков Санкт-Петербурга",
   links: "http://sqagroup.spb.ru/",
-  remote_cover_url: "http://it-sobytie.ru/system/ckeditor/pictures/5970/content_makarov.jpg",
+  # remote_cover_url: "http://it-sobytie.ru/system/ckeditor/pictures/5970/content_makarov.jpg",
   remote_logo_url: "http://piter-united.ru/img/spbsqa.png",
 )
 
@@ -44,7 +44,7 @@ Community.create!(
   hashtag: "spb-techwriters",
   description: "Санкт-Петербургское сообщество технических писателей.",
   links: "https://plus.google.com/u/0/communities/111110772662436773783",
-  remote_cover_url: "https://lh3.googleusercontent.com/-NQq_TSSpqu4/VQLkZL8VbyI/AAAAAAAAKfM/JaebgaJ07Js/w1914-h1276-no/IMG_1889.jpg",
+  # remote_cover_url: "https://lh3.googleusercontent.com/-NQq_TSSpqu4/VQLkZL8VbyI/AAAAAAAAKfM/JaebgaJ07Js/w1914-h1276-no/IMG_1889.jpg",
   remote_logo_url: "http://piter-united.ru/img/techwriters.png",
 )
 
@@ -54,7 +54,7 @@ Community.create!(
   hashtag: "ux-spb",
   description: "Сообщество проектировщиков интерфейсов и юзабилистов Санкт-Петербурга.",
   links: "http://ux-spb.ru/",
-  remote_cover_url: "https://lh3.googleusercontent.com/-lphah0FFbT4/VYAR5tPriZI/AAAAAAAABk0/2GJ79u7SKoo/w1914-h1276-no/IMG_1271.jpg",
+  # remote_cover_url: "https://lh3.googleusercontent.com/-lphah0FFbT4/VYAR5tPriZI/AAAAAAAABk0/2GJ79u7SKoo/w1914-h1276-no/IMG_1271.jpg",
   remote_logo_url: "http://piter-united.ru/img/uxspb.png",
 )
 
@@ -64,7 +64,7 @@ Community.create!(
   hashtag: "spb-frontend",
   description: "Сообщество frontend-разработчиков.",
   links: "https://vk.com/spb_frontend",
-  remote_cover_url: "https://pbs.twimg.com/media/CCLIzJNWYAENIFm.jpg",
+  # remote_cover_url: "https://pbs.twimg.com/media/CCLIzJNWYAENIFm.jpg",
   remote_logo_url: "http://piter-united.ru/img/frontend.jpg",
 )
 
@@ -74,7 +74,7 @@ Community.create!(
   hashtag: "spb-frontend",
   description: "Сообщество системных администраторов и DevOps инженеров.",
   links: "http://www.meetup.com/DevOps-40/",
-  remote_cover_url: "http://photos1.meetupstatic.com/photos/event/c/4/9/4/highres_433790324.jpeg",
+  # remote_cover_url: "http://photos1.meetupstatic.com/photos/event/c/4/9/4/highres_433790324.jpeg",
   remote_logo_url: "http://piter-united.ru/img/devops.png",
 )
 
@@ -84,7 +84,7 @@ Community.create!(
   hashtag: "it-talk",
   description: "Открытое сообщество при поддержке компании DataArt.",
   links: "http://it-talk.dataart.ru/",
-  remote_cover_url: "http://static.dataart.ru/blog/wp-content/uploads//2015/07/19753780070_f484cdefaa_k-900x507.jpg",
+  # remote_cover_url: "http://static.dataart.ru/blog/wp-content/uploads//2015/07/19753780070_f484cdefaa_k-900x507.jpg",
   remote_logo_url: "http://piter-united.ru/img/ittalk.png",
 )
 
@@ -94,7 +94,7 @@ Community.create!(
   hashtag: "sprug",
   description: "Сообщество Ruby/RoR-программистов Санкт-Петербурга.",
   links: "https://plus.google.com/u/0/communities/117927540789820622012",
-  remote_cover_url: "https://lh3.googleusercontent.com/-lphah0FFbT4/VYAR5tPriZI/AAAAAAAABk0/2GJ79u7SKoo/w1914-h1276-no/IMG_1271.jpg",
+  # remote_cover_url: "https://lh3.googleusercontent.com/-lphah0FFbT4/VYAR5tPriZI/AAAAAAAABk0/2GJ79u7SKoo/w1914-h1276-no/IMG_1271.jpg",
   remote_logo_url: "http://piter-united.ru/img/sprug.png",
 )
 
@@ -104,7 +104,7 @@ Community.create!(
   hashtag: "spb-pig",
   description: "Сообщество Python-программистов Санкт-Петербурга.",
   links: "https://plus.google.com/u/0/communities/107328406287050920928",
-  remote_cover_url: "https://lh5.googleusercontent.com/-pHRPfIW3rdg/U5P6ZAezvPI/AAAAAAAAMow/u02oRflBYu4/w1916-h1276-no/IMG_1851.jpg",
+  # remote_cover_url: "https://lh5.googleusercontent.com/-pHRPfIW3rdg/U5P6ZAezvPI/AAAAAAAAMow/u02oRflBYu4/w1916-h1276-no/IMG_1851.jpg",
   remote_logo_url: "http://piter-united.ru/img/python.png",
 )
 
@@ -114,7 +114,7 @@ Community.create!(
   hashtag: "fprog",
   description: "Ценители функционального программирования из Питера.",
   links: "https://plus.google.com/u/0/communities/114227952963737516047",
-  remote_cover_url: "http://spbit.ru/files/slet5_1434442344.jpg",
+  # remote_cover_url: "http://spbit.ru/files/slet5_1434442344.jpg",
   remote_logo_url: "http://piter-united.ru/img/clojure.png",
 )
 
@@ -124,7 +124,7 @@ Community.create!(
   hashtag: "spb-lisp",
   description: "Сообщество программистов на Lisp.",
   links: "https://vk.com/lisp.users",
-  remote_cover_url: "https://lh3.googleusercontent.com/-Vkw-rUwaX0A/Va_Hep0OiYI/AAAAAAAAYAE/AInwsY6i4lw/w800-h800/IMG_20150722_193926.jpg",
+  # remote_cover_url: "https://lh3.googleusercontent.com/-Vkw-rUwaX0A/Va_Hep0OiYI/AAAAAAAAYAE/AInwsY6i4lw/w800-h800/IMG_20150722_193926.jpg",
   remote_logo_url: "http://piter-united.ru/img/lisp.png"
 )
 
@@ -134,7 +134,7 @@ Community.create!(
   hashtag: "piter-js",
   description: "Сообщество вокруг JavaScript, платформы node.js и все, что с ними связано.",
   links: "https://vk.com/piterjs",
-  remote_cover_url: "http://riverflow.ru/images12/Piter-2011/Piter-2011more(4).jpg",
+  # remote_cover_url: "http://riverflow.ru/images12/Piter-2011/Piter-2011more(4).jpg",
   remote_logo_url: "http://piter-united.ru/img/piterjs.jpg",
 )
 
@@ -144,7 +144,7 @@ Community.create!(
   hashtag: "spb-lug",
   description: "Петербургское Linux-сообщество.",
   links: "https://vk.com/spblinuxcommunity",
-  remote_cover_url: "https://pp.vk.me/c617523/v617523464/1cf9e/5LjxA6dgWpo.jpg",
+  # remote_cover_url: "https://pp.vk.me/c617523/v617523464/1cf9e/5LjxA6dgWpo.jpg",
   remote_logo_url: "http://piter-united.ru/img/spblug.png",
 )
 
@@ -154,7 +154,7 @@ Community.create!(
   hashtag: "modx",
   description: "Клуб веб-разработчиков MODx Revolution.",
   links: "https://vk.com/modx_spb",
-  remote_cover_url: "https://pp.vk.me/c312427/v312427604/5741/qSfUEb4k7Fw.jpg",
+  # remote_cover_url: "https://pp.vk.me/c312427/v312427604/5741/qSfUEb4k7Fw.jpg",
   remote_logo_url: "http://www.yotocreative.co.uk/images/articles/modx-logo.jpg"
 )
 
@@ -164,7 +164,7 @@ Community.create!(
   hashtag: "spb-wordpress",
   description: "WordPress Meetup в Санкт-Петербурге.",
   links: "http://wpspb.org/",
-  remote_cover_url: "https://wpmag.ru/wp-content/uploads/sites/13/2014/11/wordpress-meetup-spb.jpg",
+  # remote_cover_url: "https://wpmag.ru/wp-content/uploads/sites/13/2014/11/wordpress-meetup-spb.jpg",
   remote_logo_url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSxXTIW44FU1SjDXpgON6QhGAU3dPr-sqx6oTrDAhgNdW5FFHEi"
 )
 
@@ -174,7 +174,7 @@ Community.create!(
   hashtag: "spb-startup",
   description: "Питерское стартап сообщество.",
   links: "https://www.facebook.com/groups/pitervc/",
-  remote_cover_url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xpf1/t31.0-8/11013588_642085705927252_1387376285650163046_o.jpg",
+  # remote_cover_url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xpf1/t31.0-8/11013588_642085705927252_1387376285650163046_o.jpg",
   remote_logo_url: "http://piter-united.ru/img/spbstartup.png",
 )
 
@@ -184,7 +184,7 @@ Community.create!(
   hashtag: "spb-postgresql",
   description: "Сообщество специалистов по PostgreSQL в Петербурге.",
   links: "http://pgday.ru/ru",
-  remote_cover_url: "http://www.s-p-b.pl/images/XII_WZ_01.jpg",
+  # remote_cover_url: "http://www.s-p-b.pl/images/XII_WZ_01.jpg",
   remote_logo_url: "https://s3-ap-northeast-1.amazonaws.com/nt46/logdown/2014/05/install-postgresql-934-on-mac.png",
 )
 
@@ -194,7 +194,7 @@ Community.create!(
   hashtag: "spb-it-hr",
   description: "Сообщество HR-специалистов Санкт-Петербурга.",
   links: "",
-  remote_cover_url: "http://www.nw.de/_em_daten/_cache/image/0xUmFuZG9tSVZitxHUEp6UzqRmleK4AdzNDHvlGFpOfK5QPgTuazevvARHj0QRi2AbCpvaLCvHrIj4MtsB1DYLdXALRqCFC70GzA6Spb2m4hXXR7gW0xOv8A==.jpg",
+  # remote_cover_url: "http://www.nw.de/_em_daten/_cache/image/0xUmFuZG9tSVZitxHUEp6UzqRmleK4AdzNDHvlGFpOfK5QPgTuazevvARHj0QRi2AbCpvaLCvHrIj4MtsB1DYLdXALRqCFC70GzA6Spb2m4hXXR7gW0xOv8A==.jpg",
   remote_logo_url: "http://cs619525.vk.me/v619525634/df54/dNQxK-Mry6A.jpg",
 )
 
@@ -204,7 +204,7 @@ Community.create!(
   hashtag: "spb-elearning",
   description: "e-Learning сообщество Санкт-Петербурга.",
   links: "http://www.meetup.com/e-Learning-Community-Meetups-in-Saint-Petersburg-Moodle-eLS/",
-  remote_cover_url: "http://i-elearning.ru/wordpress/wp-content/uploads/2014/02/20140203_153854_8_bestshot.jpg",
+  # remote_cover_url: "http://i-elearning.ru/wordpress/wp-content/uploads/2014/02/20140203_153854_8_bestshot.jpg",
   remote_logo_url: "http://piter-united.ru/img/elearning.jpeg"
 )
 
@@ -214,7 +214,7 @@ Community.create!(
   hashtag: "spb-dotnet",
   description: "Сообщество .NET разработчиков.",
   links: "https://plus.google.com/u/0/communities/115643868183582812348",
-  remote_cover_url: "https://lh4.googleusercontent.com/-Vkw-rUwaX0A/Va_Hep0OiYI/AAAAAAAAYA0/lWC9p40D1S4/w1702-h1276-no/IMG_20150722_193926.jpg",
+  # remote_cover_url: "https://lh4.googleusercontent.com/-Vkw-rUwaX0A/Va_Hep0OiYI/AAAAAAAAYA0/lWC9p40D1S4/w1702-h1276-no/IMG_20150722_193926.jpg",
   remote_logo_url: "http://piter-united.ru/img/netcommunity.png",
 )
 
@@ -224,7 +224,7 @@ Community.create!(
   hashtag: "spb-cio",
   description: "Клуб IT-директоров Санкт-Петербурга.",
   links: "http://www.spbcioclub.ru/",
-  remote_cover_url: "http://i.ytimg.com/vi/WR9dwR2uhVI/maxresdefault.jpg",
+  # remote_cover_url: "http://i.ytimg.com/vi/WR9dwR2uhVI/maxresdefault.jpg",
   remote_logo_url: "http://piter-united.ru/img/spbcio.gif",
 )
 
@@ -234,7 +234,7 @@ Community.create!(
   hashtag: "hackiot",
   description: "Интернет вещей воплоти.",
   links: "https://vk.com/hackiot",
-  remote_cover_url: "https://pp.vk.me/c624518/v624518362/26365/RtcxFHJov8A.jpg",
+  # remote_cover_url: "https://pp.vk.me/c624518/v624518362/26365/RtcxFHJov8A.jpg",
   remote_logo_url: "http://piter-united.ru/img/hackiot.jpg",
 )
 
@@ -244,7 +244,7 @@ Community.create!(
   hashtag: "uug-spb",
   description: "Сообщество разработчиков на Unity3d в Санкт-Петербурге.",
   links: "https://plus.google.com/u/0/communities/103652474826197512695",
-  remote_cover_url: "https://02varvara.files.wordpress.com/2010/05/bart-in-spb-05-st-isaac.jpg?w=1000&h=667",
+  # remote_cover_url: "https://02varvara.files.wordpress.com/2010/05/bart-in-spb-05-st-isaac.jpg?w=1000&h=667",
   remote_logo_url: "http://piter-united.ru/img/uug.png",
 )
 
@@ -254,7 +254,7 @@ Community.create!(
   hashtag: "3d4web",
   description: "Специалисты по 3D-моделям в WEB.",
   links: "http://www.meetup.com/3D4WEB/",
-  remote_cover_url: "http://specia.pro/images/cms/ckeditor/1.png",
+  # remote_cover_url: "http://specia.pro/images/cms/ckeditor/1.png",
   remote_logo_url: "http://piter-united.ru/img/3d4web.jpeg",
 )
 
@@ -264,7 +264,7 @@ Community.create!(
   hashtag: "spb-cloud",
   description: "Сообщество специалистов по облачным технологиями и виртуализации.",
   links: "https://vk.com/spb.cloud",
-  remote_cover_url: "https://pp.vk.me/c6074/v6074822/f9cb/CptbwcoDc_w.jpg",
+  # remote_cover_url: "https://pp.vk.me/c6074/v6074822/f9cb/CptbwcoDc_w.jpg",
   remote_logo_url: "http://piter-united.ru/img/cloud.jpg",
 )
 
@@ -274,6 +274,6 @@ Community.create!(
   hashtag: "goland-piter",
   description: "Сообщество программистов на GO.",
   links: "http://www.meetup.com/Golang-Peter/",
-  remote_cover_url: "http://i.ytimg.com/vi/GeWgU8jWVT0/maxresdefault.jpg",
+  # remote_cover_url: "http://i.ytimg.com/vi/GeWgU8jWVT0/maxresdefault.jpg",
   remote_logo_url: "http://piter-united.ru/img/goland.png",
 )
