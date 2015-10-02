3e2591c23a26974ee4ddf3c4542e433eca91c807
Andrey Zinenko <azinenko@spbtv.com>
2015-10-02 04:26:53 +0300

    Disable assets compression

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem "pg"
 gem "sass-rails", "~> 5.0"
 gem "slim-rails"
 gem "therubyracer", platforms: :ruby
-gem "uglifier", ">= 1.3.0"
+# gem "uglifier", ">= 1.3.0"
 gem "coffee-rails", "~> 4.1.0"
 gem "less-rails"
 gem "angular_rails_csrf"
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,18 @@
+class NoCompression
+  def compress(string)
+    # do nothing
+    string
+  end
+end
+
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.compress = true
+Rails.application.config.assets.js_compressor = NoCompression.new
+Rails.application.config.assets.css_compressor = NoCompression.new
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
